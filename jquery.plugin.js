bb71b91966c5e00ea473191a30821cbfd9089f00
Anthony Ettinger <anthony@chovy.com>
2013-04-24 17:02:55 -0700

    retab

--- jquery.plugin.js
+++ jquery.plugin.js
@@ -1,22 +1,22 @@
 ;(function($){
-    /*
-    * aettinger - Boiler plate for jQuery plugin
-    * $("div").myFunc({ my_option: 'some option' });
-    *
-    */
-    $.fn.extend({
-        myFunc: function( opts ) {
-            //default options
-            var opts = $.extend({
-            }, opts);
+	/*
+	* aettinger - Boiler plate for jQuery plugin
+	* $("div").myFunc({ my_option: 'some option' });
+	*
+	*/
+	$.fn.extend({
+		myFunc: function( opts ) {
+			//default options
+			var opts = $.extend({
+			}, opts);
 
-            return this.each(function(){
-                var $el = $(this);
+			return this.each(function(){
+				var $el = $(this);
 
-								//do stuff here
-								if ( opts.my_option ) {
-								}
-            });
-        }
-    });
+				//do stuff here
+				if ( opts.my_option ) {
+				}
+			});
+		}
+	});
 })(jQuery);
